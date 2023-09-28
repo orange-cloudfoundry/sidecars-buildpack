8f9f6c878c46878482bfdee26e8df53604837c1f
Romain Dartigues <romain.dartigues.ext@orange.com>
2023-09-28 11:29:04 +0200

    too many arguments in call to cutlass.InternetTraffic

--- src/sidecars/integration/integration_suite_test.go
+++ src/sidecars/integration/integration_suite_test.go
@@ -121,7 +121,6 @@ func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
 			defer os.Remove(bpFile)
 
 			traffic, built, _, err := cutlass.InternetTraffic(
-				bpDir,
 				filepath.Join("fixtures", fixtureName),
 				bpFile,
 				[]string{"HTTP_PROXY=" + proxy.URL, "HTTPS_PROXY=" + proxy.URL},
@@ -152,7 +151,6 @@ func AssertNoInternetTraffic(fixtureName string) {
 		defer os.Remove(bpFile)
 
 		traffic, built, _, err := cutlass.InternetTraffic(
-			bpDir,
 			filepath.Join("fixtures", fixtureName),
 			bpFile,
 			[]string{},
